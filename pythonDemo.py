feb3f0f41f044be5f1a5eae7eea5c480a28f477f
Basicxd <36246828+Basicxd@users.noreply.github.com>
2019-04-24 13:00:15 +0200

    mk

--- pythonDemo.py
+++ pythonDemo.py
@@ -23,13 +23,12 @@ def green():
 def yellow():
   sense.clear(255, 255, 0)
 
-# Tell the program which function to associate with which direction
 sense.stick.direction_up = red
 sense.stick.direction_down = blue
 sense.stick.direction_left = green
 sense.stick.direction_right = yellow
-sense.stick.direction_middle = sense.clear    # Press the enter key
+sense.stick.direction_middle = sense.clear 
 
 while True:
-  pass  # This keeps the program running to receive joystick events
+  pass 
   
